f7f63eca80785938fc3fb69d503031ffa1e8047d
Gio Shathaia <gioshathaia@Gios-MacBook-Air.local>
2022-12-04 12:35:49 -0500

    If applied, will generate boxes based on x and o data in a array

--- javascript.js
+++ javascript.js
@@ -1,6 +1,23 @@
-const gameBoard = (() =>[{
+const gameBoard = (() => {
+    let boxCount = ["X", "O"];
 
-}])();
+    const boxCreate = () => {
+        boxCount.map((boxCount) => {
+            let boxParent = document.querySelector(".platform");
+            let box = document.createElement("button");
+            box.classList.add("box");
+            box.textContent = boxCount;
+            boxParent.appendChild(box);
+        })
+    }
+    return {
+        publicCount: function() {
+            boxCreate();
+        }
+    }
+})();
+
+gameBoard.publicCount();
 
 const players = (name) => {
     
@@ -9,22 +26,3 @@ const players = (name) => {
 const displayController = (() => {
 
 })();
-
-const createGrid = (() => {
-    const boxCreate = () => {
-        let boxParent = document.querySelector(".platform");
-        let box = document.createElement("button");
-        box.classList.add("box");
-        boxParent.appendChild(box);
-    }
-    return {
-        publicCount: function() {
-            let boxCount = 9;
-            for (let i = 0; i < boxCount; i++) {
-                boxCreate();
-            }
-        }
-    }
-})();
-
-createGrid.publicCount();
