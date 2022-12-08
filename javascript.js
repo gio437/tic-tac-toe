26e53329b4b33ee3818a3832723e8e58f13b5b53
Gio Shathaia <gioshathaia@Gios-MacBook-Air.local>
2022-12-07 22:23:49 -0500

    If applied will now properly play the game in the console by adding the proper items to the array

--- javascript.js
+++ javascript.js
@@ -26,50 +26,45 @@ const gameBoard = (() => {
 gameBoard.publicCount();
 
 let boxCount = [];
+let xVal = 0;
 
 function markXSpot(e) {
     let num;
     let dataVal;
-    let xVal = 0;
-    console.log(e);
 
       if (e.target.classList.contains("box")) {
           num = (e.target.id);
           dataVal = parseInt(e.target.getAttribute("data-set"));
-          console.log(num);
-          console.log(dataVal);
       }
-
-      console.log(boxCount[dataVal]);
 
       let boxEl = document.querySelectorAll(".box");
         //for (let j = 0; j < boxEl.length; j++) {
-                if (boxCount[dataVal] == "X" && xVal == 0 || boxCount[dataVal] == "O" && xVal == 0) {
-                    console.log(boxCount);
-                    return;
-                }
-                else { 
-                    console.log("inputted X");
-                    boxCount[dataVal] = "X";
-                    boxEl[dataVal].id = "X";
-                    xVal = 1;
-                    markOSpot();
-                }
-
+            if (boxCount[dataVal] == "X" || boxCount[dataVal] == "O" || xVal == 1) {
                 console.log(boxCount);
-
-            function markOSpot() {
-                if (boxCount[dataVal] == "X" && xVal == 1|| boxCount[dataVal] == "O" && xVal == 1) {
-                    console.log(boxCount);
-                    return;
-                }
-                else {
-                    console.log("inputted O");
-                    boxCount[dataVal] = "O";
-                    boxEl[dataVal].id = "O";
-                    xVal = 0;
-                }
+                markOSpot();
+                return;
             }
+            else { 
+                console.log("inputted X");
+                boxCount[dataVal] = "X";
+                boxEl[dataVal].id = "X";
+                xVal = 1;
+                markOSpot();
+            }
+
+        function markOSpot() {
+            if (boxCount[dataVal] == "X" || boxCount[dataVal] == "O" || xVal == 0) {
+                console.log(boxCount);
+                return;
+            }
+            else {
+                console.log("inputted O");
+                boxCount[dataVal] = "O";
+                boxEl[dataVal].id = "O";
+                xVal = 0;
+                console.log(boxCount);
+            }
+        }
 }
 
 const players = (name) => {
