dcecf6f067d264621a4d00e837d77e3d11327b92
Gio Shathaia <gioshathaia@Gios-MacBook-Air.local>
2022-12-08 19:19:14 -0500

    If applied will allow you to input player names

--- javascript.js
+++ javascript.js
@@ -32,6 +32,16 @@ function markXSpot(e) {
     let num;
     let dataVal;
 
+    deleteResult();
+
+    function deleteResult() {
+        if (resultVal == 1) {
+            let removeButton1 = document.querySelector("h2");
+            removeButton1.remove();
+            resultVal = 0;
+        }
+    }
+
       if (e.target.classList.contains("box")) {
           num = (e.target.id);
           dataVal = parseInt(e.target.getAttribute("data-set"));
@@ -50,6 +60,13 @@ function markXSpot(e) {
                 boxEl[dataVal].id = "X";
                 xVal = 1;
                 markOSpot();
+
+                let removeBox = document.querySelectorAll(".box");
+                for (let i = 0; i < removeBox.length; i++) {
+                    //removeBox[dataVal].remove();
+                }
+
+                decideWinner();
             }
 
         function markOSpot() {
@@ -63,17 +80,126 @@ function markXSpot(e) {
                 boxEl[dataVal].id = "O";
                 xVal = 0;
                 console.log(boxCount);
+
+                let removeBox = document.querySelectorAll(".box");
+                for (let i = 0; i < removeBox.length; i++) {
+                    //removeBox[dataVal].remove();
+                }
+
+                decideWinner();
             }
         }
 }
 
-const players = (name) => {
-    
+function decideWinner(player1, player2) {
+
+    if (boxCount[0] && boxCount[1] && boxCount[2] == "X") {
+        console.log("player1 wins!");
+        players(player1);
+        boxCount = [];
+        xVal = 0;
+    }
+    else if (boxCount[0] && boxCount[3] && boxCount[6] == "X") {
+        players(player1);
+        boxCount = [];
+        xVal = 0;
+    }
+    else if (boxCount[2] && boxCount[4] && boxCount[6] == "X") {
+        players(player1);
+        boxCount = [];
+        xVal = 0;
+    }
+    else if (boxCount[3] && boxCount[4] && boxCount[5] == "X") {
+        players(player1);
+        boxCount = [];
+        xVal = 0;
+    }
+    else if (boxCount[6] && boxCount[7] && boxCount[8] == "X") {
+        players(player1);
+        boxCount = [];
+        xVal = 0;
+    }
+    else if (boxCount[1] && boxCount[4] && boxCount[7] == "X") {
+        players(player1);
+        boxCount = [];
+        xVal = 0;
+    }
+    else if (boxCount[2] && boxCount[5] && boxCount[8] == "X") {
+        players(player1);
+        boxCount = [];
+        xVal = 0;
+    }
+    else if (boxCount[0] && boxCount[4] && boxCount[8] == "X") {
+        players(player1);
+        boxCount = [];
+        xVal = 0;
+    }
+    else if (boxCount[0] && boxCount[1] && boxCount[2] == "O") {
+        players(player2);
+        boxCount = [];
+        xVal = 0;
+    }
+    else if (boxCount[0] && boxCount[3] && boxCount[6] == "O") {
+        players(player2);
+        boxCount = [];
+        xVal = 0;
+    }
+    else if (boxCount[2] && boxCount[4] && boxCount[6] == "O") {
+        players(player2);
+        boxCount = [];
+        xVal = 0;
+    }
+    else if (boxCount[3] && boxCount[4] && boxCount[5] == "O") {
+        players(player2);
+        boxCount = [];
+        xVal = 0;
+    }
+    else if (boxCount[6] && boxCount[7] && boxCount[8] == "O") {
+        players(player2);
+        boxCount = [];
+        xVal = 0;
+    }
+    else if (boxCount[1] && boxCount[4] && boxCount[7] == "O") {
+        players(player2);
+        boxCount = [];
+        xVal = 0;
+    }
+    else if (boxCount[2] && boxCount[5] && boxCount[8] == "O") {
+        players(player2);
+        boxCount = [];
+        xVal = 0;
+    }
+    else if (boxCount[0] && boxCount[4] && boxCount[8] == "O") {
+        players(player2);
+        boxCount = [];
+        xVal = 0;
+    }
+}
+
+let getPlayer1 = document.querySelector(".btn1");
+getPlayer1.addEventListener("click", () => {
+    let player1 = window.prompt("enter player 1");
+    decideWinner(player1);
+})
+
+let getPlayer2 = document.querySelector(".btn2");
+getPlayer2.addEventListener("click", () => {
+    let player2 = window.prompt("enter player 2");
+    decideWinner(player2);
+})
+
+let resultVal = 0;
+
+const players = (player1, player2) => {
+    let winnerLine = document.querySelector(".result");
+    let winHeader = document.createElement("h2");
+    winHeader.textContent = player1 + "wins!";
+    winHeader.textContent = player2 + "wins!";
+    winnerLine.appendChild(winHeader);
+    resultVal++;
 };
 
-const getPlayer = players();
-
-//getPlayer();
+//players();
 
 const displayController = (() => {
 
